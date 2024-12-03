f77cdd4939bf1dc7aff9e614950d78999ffd88b4
omarAlexis1999 <osanmartint3d@gmail.com>
2024-12-03 00:57:32 -0500

    Delete Controller and Routes - previous logic

--- src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Rutas de autenticación
-router.post('/login', authController.login);
-
-module.exports = router;
